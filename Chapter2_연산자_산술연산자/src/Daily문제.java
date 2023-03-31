a29c9edeca5096aa229962a7d56e35ef4ba2f457
SIST <SIST@DESKTOP-QBIB7KF>
2023-03-31 09:06:23 +0900

    1111

---
+++ Chapter2_연산자_산술연산자/src/Daily문제.java
@@ -0,0 +1,79 @@
+/*
+ * import java.util.Scanner;
+ * Scanner sc = new Scanner(System.in); //System.in ->키보드 입력
+ * nextInt()는 공백기준으로 끊어어 입력을 받음 -> 한번에 여러개 입려가능
+ * int a = (int)(Math.Random()*65)
+ * 
+ * printf() ->  jdk1.5이상 부서 사용가능
+ * System.out.println("a=" + a + ",d=" + d + ",c=" + c) 
+ * System.out.printf("a=%3d b=4.1f c=%2c",a,d,c)
+ * 모든 프로그램은 1.변수선언 : int a,b 2. 초기화 :Scanner 3. 연산처리 : a-b 4.결과출력
+ * 
+ * 
+ */
+import java.util.Scanner;
+public class Daily문제 {
+	
+	public static void main(String[] args) {
+//      //12번문제		 
+//		Scanner scan = new Scanner(System.in);
+//		System.out.println("두개의 정수를 입력해주세요 : (하나씩 입력)");
+//		int a = scan.nextInt();
+//		int b = scan.nextInt();
+//		
+//		int sub = a-b;
+//		int mul = a*b;
+//		System.out.printf("뺼셈의 결과는 %d, 곱셈의 결과는%d 입니다\n",sub,mul);
+		
+////  13번 문제
+//		Scanner scan = new Scanner(System.in);
+//		System.out.println("세개의 정수를 입력해 주세요 : ");
+		
+//		int a = scan.nextInt();
+//		int b = scan.nextInt();
+//		int c = scan.nextInt();
+//		int total = (a*b)+c;
+//		
+//		System.out.printf("%d*+%d+%d",a,b,c);
+		
+//14번 문제
+		
+//	Scanner scan = new Scanner(System.in);
+//	System.out.println("두개의 정수를 입력해 주세요 : ");
+//	int a = scan.nextInt();
+//	int b=scan.nextInt();
+//	int div = a%b;
+//	System.out.pritln(div);
+//	System.out.println("a=%d, b=%d, a%%b=%d",a,b,a%b); //%를 출력할려면 %%두번써주면 된다.
+		
+			
+//번외
+//		int age = 0;
+//		String name = "";
+//		String x,y = "";
+//		Scanner sc = new Scanner(System.in);
+//		
+//		//nextInt()와 nextLine()
+//		System.out.print("나이를 입력하세요 : ");
+//		age = sc.nextInt(); // 정수를 입력받음
+//		System.out.print("이름을 입력하세요 : ");
+//		name = sc.next(); // 공백 전 까지 입력 받음
+//		System.out.printf("나이 : %d  이름 : %s%n%n",age, name);
+//		
+//		// next() 와 nextLine()의 차이점
+//		System.out.print("a b c d 입력 : ");
+//		x = sc.next();	
+//		y = sc.nextLine(); // 한 줄 전체를 입력받음 
+//		System.out.printf("x = %s%ny = %s",x,y);
+//		//System.out.printf("x = %s %ny = %s %n",x, y);
+	
+	
+		
+		
+		
+
+		
+	}
+}
+		
+	
