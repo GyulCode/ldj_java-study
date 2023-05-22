c5f48c14932975f052d79d39f8ccfd3ba68096c8
GyulCode <ldg2057@naver.com>
2023-05-22 17:49:11 +0900

    staged change and commit and push

--- MemoList/src/MyMemo/My_Memo.java
+++ MemoList/src/MyMemo/My_Memo.java
@@ -4,55 +4,23 @@ package MyMemo;
  23-05-15
  --study-
  
- 포폴 만들어두기(소켓, 크롤링), 자바 복습(스트림버퍼, 제네릭, 소켓..등등),목표 및 로드맵 구체화
+ 방콕 일정 전체 검토(환전, 유심) 및 준비물 확인, 기념품 리스트 , 면세점
+ 깃, 노션 공부
+ -> 남으면 cs, 자바, 로드맵(요구사항 분석 및 필요 기술)
  
-금 : jdk 글쓰기, 깃공부, 노션
-토 : 디비버 자료연결, 
-일 : 교재진도 공부, 옷장정리 -> 구매릿트, 방콕 일정 전체 검토(환전, 유심) 및 준비물 확인, 기념품 리스트 , 면세점, 
+ 포폴 만들어두기(소켓, 크롤링), 자바 복습(스트림버퍼, 제네릭, 소켓..등등)
+ 옷장정리 -> 구매릿트,  
 
 --개인--
 여행 준비 : 면세, 옷, 기념품리스트
 개발자 : 자바 공부(부족한거)
 
-
-
-            복습 page : 145(select형식), 149(연산자 종류), 157(group by 집계함수),
-                       161(group by 주의점, 컬럼명과 집계합수 동시사용 불가)
-                       162 조인, 169 조인형식
-                       170 서브쿼리 173 확인(서브쿼리 실행순서)
-                       179 DDL create 형식, 순서
-                       180 page : 문자데이터형
-                       183 오라클 데이터형
-                       184~185 alter : add, modify, drop
-                       186 : drop
-
+-시간복잡도
+https://hanamon.kr/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-time-complexity-%EC%8B%9C%EA%B0%84-%EB%B3%B5%EC%9E%A1%EB%8F%84/
+-점근 표기법
+https://ratsgo.github.io/data%20structure&algorithm/2017/09/13/asymptotic/
 */
 
 
-/* 
-   자바의 다형성, 오라클=index
-   JVM<JRE<JDK
-   이란 뭘까??
-   JVM(Java Virtual Machine) : 자바와 운영체제 사이에서 중개자 역할을 해주는 소프트웨어로 운영체제의 종류에 구에 받지 않고
-   프로그램을 실행할 수 있도록 도와줍니다
-   
-   구조
-   .java(자바코드) -> javac(컴파일러) -> .class(바이트코드)
-   이걸 각각의 환경에 맞게 설치된 jvm에 전달 실행
-   
-   gc로 자동 메모리 관리도 해준다
-   레지스터 기반이 아니라 스택 기반
-   jVM, 메모리구조
-   https://steady-coding.tistory.com/305
-   -runtime data area(메모리)
-   https://www.holaxprogramming.com/2013/07/16/java-jvm-runtime-data-area/
-   -프로세스와 스레드의 개념
-   http://www.tcpschool.com/java/java_thread_concept
-   
-   JRE
-   
-   JDK
-   
-   
- */
+
 
--- MemoList/src/Oracle_Memo/test_0522.java
+++ MemoList/src/Oracle_Memo/test_0522.java
@@ -1,5 +1,19 @@
 package Oracle_Memo;
-
+/*
+    rownum / 서브쿼리 / view
+    1. rownum : 오라클에서 지원하는 가상 컬럼
+                row마다 번호를 순차적으로 설정함 --> insert된 순서로 지정
+       사용처 : TOP-N 인기순위 10개, 최신수 10개..., 게시판 이전 다음 내용보기
+              페이징 기법 
+              
+              
+              
+ 
+    
+    
+    
+    
+ */
 public class test_0522 {
 
 }
