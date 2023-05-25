10756ae4940a1e6edeffb2c0f089d656e32dee58
GyulCode <ldg2057@naver.com>
2023-05-25 17:52:19 +0900

    0525_commit

--- MemoList/src/MyMemo/My_Memo.java
+++ MemoList/src/MyMemo/My_Memo.java
@@ -3,9 +3,10 @@ package MyMemo;
 /*
  23-05-15
  --study-
- 1. 혼공책 끝
- 2. 웹동작 원리
- 3. 깃(브랜치), 노션 공부
+ 1. 웹동작 원리, 메모리 영역
+ 2. 깃(브랜치), 노션 공부
+ 3. 진도 복습 html, 데이터 베이스 
+ 4. 자바(소켓, 스트림, 제네릭)
  쇼핑리스트(면세, 옷), 환전(목)
  개발자(로드맵), 자바 
 
@@ -19,7 +20,11 @@ package MyMemo;
  --취업 :
  1. 포폴 만들어두기(소켓, 크롤링)
 
-FullStack(Spring + VueJS, ReactJS => redux / Vue3)
+Front-End : JavaScript(Jquery(Ajax),VueJS,ReactJS,NodeJS)
+Back-End : Java, Oracle, Spring, MSA(분산처리)
+                                       |  [도커, 쿠바네티스, 젠킨스]
+                              MVC -> Domain
+FullStack : Front + Back -> (Spring + VueJS, ReactJS => redux / Vue3)
 
 */
 
