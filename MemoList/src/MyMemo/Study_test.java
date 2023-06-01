65ef1a68354933a31ef2457b9e5d7065573bc83a
GyulCode <ldg2057@naver.com>
2023-06-01 17:00:48 +0900

    0601_마감

--- MemoList/src/MyMemo/Study_test.java
+++ MemoList/src/MyMemo/Study_test.java
@@ -8,30 +8,48 @@ public class Study_test {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	static int[] cardnum ;
-	int compare(int arr[], int n, int m) {
-		
-		int a=1;
-		return a;
+	static int temp=0;
+	
+	static int compare(int arr[], int n, int m) {
+		int answer = 0;
+		for(int i=0; i<n-2; i++) {
+			for(int j=i+1; j<n-1;j++) {
+				for(int k=j+1; k<n; k++) {
+					temp = arr[i]+arr[j]+arr[k];
+					
+					if(m==temp) {
+						return temp;
+					}
+					if(answer<temp && temp<m) {
+						answer=temp;
+					}
+					
+				}
+			}
+		}
+		return answer;
 	}
 	 
 	public static void main(String[] args) {
-		
+		 
 		try {
-			st = new StringTokenizer(br.readLine());
-			int n = Integer.parseInt(st.nextToken());
-			int m = Integer.parseInt(st.nextToken());
+			st = new StringTokenizer(br.readLine()); 
+			int n = Integer.parseInt(st.nextToken()); // 카드 갯수는 3~100
+			int m = Integer.parseInt(st.nextToken()); // 카드의 합은 10~300,000
 			
-			cardnum = new int[n];                  
-			st = new StringTokenizer(br.readLine());
+			cardnum = new int[n];
+			
+			st = new StringTokenizer(br.readLine()); //진열될 카드의 값들 각값은 100,000이하
 			for(int i=0; i<n; i++) {
 				cardnum[i] = Integer.parseInt(st.nextToken());
 			}
 			
-		} catch (Exception ex) {
+			int answer=compare(cardnum,n,m);
+			System.out.println(answer);
 			
-		}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} 
 	}
-	
-    
     
 }
---
+++ MemoList/src/MyMemo/algorithm_study.java
@@ -0,0 +1,13 @@
+package MyMemo;
+
+public class algorithm_study {
+
+	public static void main(String[] args) {
+		// 약수 찾기 -> 약수들의 합 비교 -> 맞으면 출력
+		// -1 입력시 종료
+		
+		
+
+	}
+
+}
