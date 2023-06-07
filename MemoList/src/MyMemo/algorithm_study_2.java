70462df91c51de15916cfb6d44b0fa7e7b7de556
GyulCode <ldg2057@naver.com>
2023-06-07 13:57:44 +0900

    0607-메모수정

--- MemoList/src/MyMemo/algorithm_study_2.java
+++ MemoList/src/MyMemo/algorithm_study_2.java
@@ -2,7 +2,7 @@ package MyMemo;
 
 import java.io.*;
 
-public class Main {
+public class algorithm_study_2 {
 
 	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
