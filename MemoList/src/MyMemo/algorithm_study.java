ef852a0825366bd35a270ace55487bc41c506d15
GyulCode <ldg2057@naver.com>
2023-06-16 16:54:40 +0900

    0616

--- MemoList/src/MyMemo/algorithm_study.java
+++ MemoList/src/MyMemo/algorithm_study.java
@@ -1,12 +1,33 @@
 package MyMemo;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class algorithm_study {
 
 	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	static int ZeroCount(int n, int m) {
+		int count =0;
+		for(int i=n;n<=m;n++) {
+			
+		}
+		
+		return count;
+	}
 	
+	public static void main(String[] args) throws IOException {
+		int number = Integer.parseInt(in.readLine());
+		while(number > 0) {
+			st = new StringTokenizer(in.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			
+			
+			number--;
+		}
+	}
 	
      
 }
