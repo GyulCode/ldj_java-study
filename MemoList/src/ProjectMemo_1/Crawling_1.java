0ff15633e3b872bf892c34cb6b7624fde4d998b1
GyulCode <ldg2057@naver.com>
2023-06-23 17:47:36 +0900

    크롤링연습

--- MemoList/src/ProjectMemo_1/Crawling_1.java
+++ MemoList/src/ProjectMemo_1/Crawling_1.java
@@ -5,17 +5,22 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 public class Crawling_1 {
+	//마이펫 #container .list .subject
+	//견이랑 : .item-list3 .info .sbj
 	//크롤링
 		public static void main(String[] args) {
 			
 			try {
 				// 사이트 연결
-				String url="https://gyeoniyrang.com/_subpage/kor/product/hospital.php?ca_id=76&dateStart=&dateEnd=&page=";
+				String url="https://bemypet.kr/lounge/";
 				
-				for(int i=1; i<=10; i++) {
+				for(int i=76035; i<=76045; i++) {
 					String url1=url+Integer.toString(i);
 					Document doc=Jsoup.connect(url1).get();
-					Elements title=doc.select(".item-list3 .info .sbj");
+					Elements title=doc.select(".qaDetail_qaDetailQuestion__AXVqt .qaDetail_qaDetailTop__O4CyB");
+					Elements cont=doc.select(".qaDetail_qaDetailQuestion__AXVqt .qaDetail_qaDetailTop__O4CyB");
+					Elements img=doc.select(".qaDetail_qaDetailQuestion__AXVqt .qaDetail_qaDetailTop__O4CyB");
+					Elements id=doc.select(".qaDetail_qaDetailQuestion__AXVqt .qaDetail_qaDetailTop__O4CyB");
 					for(int j=0; j<title.size();j++) {
 						System.out.println("제목:"+title.get(j).text());
 						System.out.println("----------------------------------");
