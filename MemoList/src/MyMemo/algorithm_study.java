7c56dc03f547699922178d96410b3d90694840fe
GyulCode <ldg2057@naver.com>
2023-06-27 18:01:36 +0900

    알고리즘 라파벨롬

--- MemoList/src/MyMemo/algorithm_study.java
+++ MemoList/src/MyMemo/algorithm_study.java
@@ -1,7 +1,10 @@
 package MyMemo;
 
-import java.io.*;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
 /*
  * 배열갯수 N 반복횟수 M
@@ -10,6 +13,42 @@ import java.util.StringTokenizer;
  */
 
 public class algorithm_study {
+	
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int n;
+	
+	public static void main(String[] args) {
+		try {
+			
+			String a= in.readLine();
+			n = a.length();
+			int ans=1;
+			char com[] = new char[n];
+			
+			for(int i=0; i<n; i++) {
+				com[i] = a.charAt(i);
+			}
+			int k = n;
+			// 012345
+			// 05 14 23
+			//01234
+			//04 13 
+			 //같은지 다른지 검사
+			 for(int i=0; i<n/2;i++) {
+				 if(com[i]!=com[(n-1)-i]) {
+					 ans=0;
+					 break;
+				 }
+			 }
+			 out.write(Integer.toString(ans));
+			 out.flush();
+
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
 
 }
